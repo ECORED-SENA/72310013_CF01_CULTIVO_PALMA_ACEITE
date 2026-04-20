52fc777349cab6f75628036291e0dba62afac88d
Cristian_ <cfmartinezs@hotmail.com>
2026-04-20 16:09:11 -0500

    update

--- src/config/global.js
+++ src/config/global.js
@@ -118,7 +118,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/72310013_CF01_DU.zip',
+        download: 'downloads/72310013_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
